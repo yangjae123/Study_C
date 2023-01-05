342756fbfaa77d0d0330ba08b64ddb925e5724a1
yangjae123 <yangjh1108@naver.com>
2023-01-05 22:51:51 +0900

    1일차 공부

---
+++ 윤성우 C언어/윤성우 C언어/문제 02_1_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("È«±æµ¿ \nÈ« ±æ µ¿ \nÈ«  ±æ  µ¿ \n");
+	return 0;
+}
---
+++ 윤성우 C언어/윤성우 C언어/문제 02_1_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("æÁ¿Á«ı\n");
+	printf("∂±¿Ÿ∏∂¿ª\n");
+	printf("010-1111-2222\n");
+	
+	return 0;
+}
