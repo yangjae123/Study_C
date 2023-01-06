407d069e42d10b6d11658097b0878a1f69e13334
yangjae123 <yangjh1108@naver.com>
2023-01-06 21:12:21 +0900

    2일차 공부

---
+++ 윤성우 C언어/윤성우 C언어/문제 02_2_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("제 이름은 양재혁입니다.\n");
+	printf("제 나이는 %d살이고요.\n", 23);
+	printf("제가 사는 곳의 번지수는 %d-%d입니다.", 123 , 456);
+	return 0;	
+}
---
+++ 윤성우 C언어/윤성우 C언어/예제 02_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("My age: %d \n", 23);
+	printf("%d is my point \n", 100);
+	printf("Good \nmorning \neverybody\n");
+	return 0;
+}
